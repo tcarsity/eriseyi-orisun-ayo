3c51bb1decf8e7290a31344624874680d85394d1
unknown <you@example.com>
2025-12-01 17:54:04 +0100

    Adjusting css

--- src/components/admin/NotFoundPage.jsx
+++ src/components/admin/NotFoundPage.jsx
@@ -6,9 +6,9 @@ export default function NotFoundPage() {
     <section className="section-none py-5">
       <div className="container py-5 d-flex justify-content-center align-items-center vh-100">
         <div className="row position-fixed top=50 start-50 translate-middle">
-          <div className="col">
-            <div className="text-center not-found ">
-              <div className="card border-0 shadow " style={{ width: "25rem" }}>
+          <div className="col-md-4">
+            <div className="text-center not-found">
+              <div className="card border-0 shadow" style={{ width: "25rem" }}>
                 <div className="card-body p-4">
                   <h1>404</h1>
                   <p>👻 Oops! The page you are looking for does not exist.</p>
