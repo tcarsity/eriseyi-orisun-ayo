90d11bdadd7c68374bd6a9d37d4e045cd9a8f279
unknown <you@example.com>
2025-12-10 21:31:48 +0100

    Removed content-type

--- src/api/axios.js
+++ src/api/axios.js
@@ -15,10 +15,6 @@ api.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${token}`;
   }
 
-  if (config.data instanceof FormData) {
-    delete config.headers["Content-Type"];
-  }
-
   return config;
 });
 
