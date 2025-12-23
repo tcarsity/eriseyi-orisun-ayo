26dc4dbba43595b7217a31c35ac07314f6ada8d4
unknown <you@example.com>
2025-12-23 13:27:34 +0100

    Preloader switch to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/pages/Home";
 import AddMember from "./components/pages/AddMember";
@@ -25,89 +25,8 @@ import ForgotPassword from "./components/admin/ForgotPassword";
 import ResetPassword from "./components/admin/ResetPassword";
 import EditProfile from "./components/admin/EditProfile";
 import { ThemeProvider } from "./components/context/ThemeContext";
-import Preloader from "./components/Preloader";
-import api from "./api/axios";
-import { useQueryClient } from "@tanstack/react-query";
 
 function App() {
-  const queryClient = useQueryClient();
-  const [appReady, setAppReady] = useState(false);
-
-  useEffect(() => {
-    const prepareApp = async () => {
-      try {
-        await Promise.all([
-          queryClient.prefetchQuery({
-            queryKey: ["publicTestimonials"],
-            queryFn: () =>
-              api.get("/public-testimonials").then((res) => res.data),
-          }),
-          queryClient.prefetchQuery({
-            queryKey: ["publicEvents"],
-            queryFn: () => api.get("/public-events").then((res) => res.data),
-          }),
-
-          // queryClient.prefetchQuery({
-          //   queryKey: ["adminActivities"],
-          //   queryFn: () => api.get("/admin/activities").then((res) => res.data),
-          // }),
-
-          // queryClient.prefetchQuery({
-          //   queryKey: ["adminPerformance"],
-          //   queryFn: () =>
-          //     api.get("/admin/activities/performance").then((res) => res.data),
-          // }),
-
-          // queryClient.prefetchQuery({
-          //   queryKey: ["adminStatus"],
-          //   queryFn: () => api.get("/admin-status").then((res) => res.data),
-          // }),
-
-          // queryClient.prefetchQuery({
-          //   queryKey: ["dashboardStats"],
-          //   queryFn: () => api.get("/dashboard-stats").then((res) => res.data),
-          // }),
-
-          // queryClient.prefetchQuery({
-          //   queryKey: ["recent-members"],
-          //   queryFn: () =>
-          //     api.get("recent-public-members").then((res) => res.data),
-          // }),
-
-          // queryClient.prefetchQuery({
-          //   queryKey: ["securityLogs"],
-          //   queryFn: () => api.get("/security-logs").then((res) => res.data),
-          // }),
-
-          // queryClient.prefetchQuery({
-          //   queryKey: ["testimonials"],
-          //   queryFn: () => api.get("/testimonials").then((res) => res.data),
-          // }),
-
-          // queryClient.prefetchQuery({
-          //   queryKey: ["members"],
-          //   queryFn: () => api.get("/members").then((res) => res.data),
-          // }),
-
-          // queryClient.prefetchQuery({
-          //   queryKey: ["users"],
-          //   queryFn: () => api.get("/users").then((res) => res.data),
-          // }),
-        ]);
-      } catch (err) {
-        console.error("App init error:", err);
-      } finally {
-        setAppReady(true);
-      }
-    };
-
-    prepareApp();
-  }, [queryClient]);
-
-  if (!appReady) {
-    return <Preloader />;
-  }
-
   return (
     <>
       <ThemeProvider>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useEffect, useState } from "react";
 import Layout from "../common/Layout";
 import Hero from "../common/Hero";
 import About from "./About";
@@ -6,8 +6,89 @@ const Service = lazy(() => import("./Service"));
 const Shepherd = lazy(() => import("./Shepherd"));
 const Testimonial = lazy(() => import("./Testimonial"));
 import EventsSection from "./EventsSection";
+import Preloader from "../Preloader";
+import { useQueryClient } from "@tanstack/react-query";
+import api from "../../api/axios";
 
 const Home = () => {
+  const queryClient = useQueryClient();
+  const [appReady, setAppReady] = useState(false);
+
+  useEffect(() => {
+    const prepareApp = async () => {
+      try {
+        await Promise.all([
+          queryClient.prefetchQuery({
+            queryKey: ["publicTestimonials"],
+            queryFn: () =>
+              api.get("/public-testimonials").then((res) => res.data),
+          }),
+          queryClient.prefetchQuery({
+            queryKey: ["publicEvents"],
+            queryFn: () => api.get("/public-events").then((res) => res.data),
+          }),
+
+          // queryClient.prefetchQuery({
+          //   queryKey: ["adminActivities"],
+          //   queryFn: () => api.get("/admin/activities").then((res) => res.data),
+          // }),
+
+          // queryClient.prefetchQuery({
+          //   queryKey: ["adminPerformance"],
+          //   queryFn: () =>
+          //     api.get("/admin/activities/performance").then((res) => res.data),
+          // }),
+
+          // queryClient.prefetchQuery({
+          //   queryKey: ["adminStatus"],
+          //   queryFn: () => api.get("/admin-status").then((res) => res.data),
+          // }),
+
+          // queryClient.prefetchQuery({
+          //   queryKey: ["dashboardStats"],
+          //   queryFn: () => api.get("/dashboard-stats").then((res) => res.data),
+          // }),
+
+          // queryClient.prefetchQuery({
+          //   queryKey: ["recent-members"],
+          //   queryFn: () =>
+          //     api.get("recent-public-members").then((res) => res.data),
+          // }),
+
+          // queryClient.prefetchQuery({
+          //   queryKey: ["securityLogs"],
+          //   queryFn: () => api.get("/security-logs").then((res) => res.data),
+          // }),
+
+          // queryClient.prefetchQuery({
+          //   queryKey: ["testimonials"],
+          //   queryFn: () => api.get("/testimonials").then((res) => res.data),
+          // }),
+
+          // queryClient.prefetchQuery({
+          //   queryKey: ["members"],
+          //   queryFn: () => api.get("/members").then((res) => res.data),
+          // }),
+
+          // queryClient.prefetchQuery({
+          //   queryKey: ["users"],
+          //   queryFn: () => api.get("/users").then((res) => res.data),
+          // }),
+        ]);
+      } catch (err) {
+        console.error("App init error:", err);
+      } finally {
+        setAppReady(true);
+      }
+    };
+
+    prepareApp();
+  }, [queryClient]);
+
+  if (!appReady) {
+    return <Preloader />;
+  }
+
   return (
     <>
       <Layout showAllLinks>
