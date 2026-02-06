24246904463469ed6ccad3320498c6cb50bf5882
unknown <you@example.com>
2026-02-06 09:53:55 +0100

    tested ok

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  timeout: 1000,
+  timeout: 10000,
   headers: {
     Accept: "application/json",
   },
