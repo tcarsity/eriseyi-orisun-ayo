6ddad23b037fc47ef19fdc325781240cdd418787
unknown <you@example.com>
2026-02-16 23:49:28 +0100

    error password fix

--- src/components/admin/ResetPassword.jsx
+++ src/components/admin/ResetPassword.jsx
@@ -112,10 +112,10 @@ const ResetPassword = () => {
                         {...register("password", {
                           required: "The password field is required",
 
-                          minLength: {
-                            value: 8,
-
-                            message: "Minimum 8 characters",
+                          pattern: {
+                            value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/,
+                            message:
+                              "Password must contain at least 8 characters, one uppercase letter, one lowercase letter, and one number",
                           },
                         })}
                         type="password"
